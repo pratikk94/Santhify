698159c4133ed676b046a3ce22d6322956898239
pratik <pratik@Pratiks-MacBook-Pro-New.local>
2024-12-08 06:27:42 +0530

    Filterbar update

--- src/components/Dashboard/FiltersBar/FiltersBar.tsx
+++ src/components/Dashboard/FiltersBar/FiltersBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react' 
 import { Input, Button, Space, Typography, Dropdown, Menu } from 'antd';
 import {
   SearchOutlined,
